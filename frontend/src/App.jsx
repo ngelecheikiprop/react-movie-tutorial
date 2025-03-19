5c75fc3830afca1734711d9a625cbee6f9bd23f8
ngelecheikiprop <ngelecheikiprop@gmail.com>
2025-03-19 08:24:48 +0300

    do not add modeules folder

---
+++ frontend/src/App.jsx
@@ -0,0 +1,19 @@
+
+import './App.css'
+
+function App() {
+  return (
+<>
+<Text/>
+<Text/>
+
+</>cd
+  )
+}
+function Text({text}){
+  return (
+    <div>Hello World</div>
+  );
+}
+
+export default App
