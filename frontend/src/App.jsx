02cc99983aac2b4b72bbf4c71319ba0df6916a0c
ngelecheikiprop <ngelecheikiprop@gmail.com>
2025-03-20 07:53:18 +0300

    routing and page navigation

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -4,14 +4,16 @@ import MovieCard from "./components/MovieCard"
 import {Routes, Route} from "react-router-dom"
 import Home from "./pages/Home"
 import Favorite from './pages/Favorites'
-
-function App() {
+import NavBar from "./components/NavBar"
+function App() { 
   return (
   <main className="main-content">
+     <NavBar></NavBar>
     <Routes>
       <Route path="/" element={<Home/>}/>
       <Route path="/favorites" element={<Favorite/>}/>
     </Routes>
+   
   </main>
   )
 }
